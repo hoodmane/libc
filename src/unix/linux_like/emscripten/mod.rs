d258eaa9733313bf0bf02fc507cb38820b929b72
Hood Chatham <roberthoodchatham@gmail.com>
2024-01-29 16:27:06 -0800

    time_t is 64 bits on Emscripten
    Since July 8, 2022:
    https://github.com/emscripten-core/emscripten/pull/17401
    https://github.com/emscripten-core/emscripten/blob/main/system/lib/libc/musl/arch/emscripten/bits/alltypes.h?plain=1#L83

--- src/unix/linux_like/emscripten/mod.rs
+++ src/unix/linux_like/emscripten/mod.rs
@@ -16,7 +16,7 @@ pub type loff_t = i64;
 pub type pthread_key_t = ::c_uint;
 
 pub type clock_t = c_long;
-pub type time_t = c_long;
+pub type time_t = i64;
 pub type suseconds_t = c_long;
 pub type ino_t = u64;
 pub type off_t = i64;
